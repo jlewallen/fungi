38bf3bd63ab934a2d5f9857dc4ec42918d2bfb6e
Jacob Lewallen <jlewallen@gmail.com>
2021-10-15 16:15:15 -0700

    feat: Receiving station UDP.

--- src/discovery.ts
+++ src/discovery.ts
@@ -1,11 +1,13 @@
 import Zeroconf from 'react-native-zeroconf';
+import dgram from 'react-native-udp';
 
 export class Registration {
   constructor(
     public readonly name: string,
     public readonly addresses: string[],
     public readonly port: number,
-    public seen: Date | null = null,
+    public zeroconf: Date | null = null,
+    public udp: Date | null = null,
     public lost: Date | null = null,
   ) {}
 }
@@ -46,6 +48,16 @@ export class Discovery {
 
     this.zeroconf.scan('fk', 'tcp', 'local.');
 
+    const socket = dgram.createSocket('udp4');
+    socket.bind(22143, function () {
+      console.log('udp: bound');
+      socket.addMembership('224.1.2.3');
+    });
+
+    socket.on('message', function (msg, rinfo) {
+      console.log('udp: message', msg, rinfo);
+    });
+
     setInterval(() => {
       callback(this.getServices());
     }, 1000);
@@ -55,7 +67,10 @@ export class Discovery {
 
   onServiceFound(name: string, addresses: string[], port: number) {
     console.log('onServiceFound', name, addresses, port);
-    this.services[name] = new Registration(name, addresses, port, new Date());
+    if (!this.services[name]) {
+      this.services[name] = new Registration(name, addresses, port);
+    }
+    this.services[name].zeroconf = new Date();
   }
 
   onServiceLost(name: string) {
