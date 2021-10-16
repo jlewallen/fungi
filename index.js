fb6fce62d8fe48e68acd08d0aac8cf503e9fd792
Jacob Lewallen <jlewallen@gmail.com>
2021-10-16 09:59:50 -0700

    feat: Ignore cycle warning in rn-fetch-blob.

--- index.js
+++ index.js
@@ -2,7 +2,14 @@
  * @format
  */
 
-import { AppRegistry } from "react-native";
+import { AppRegistry, LogBox } from "react-native";
+
+LogBox.ignoreLogs([
+    // "Remote debugger is in a background tab which may cause apps to perform slowly",
+    "Require cycle: node_modules/react-native-fetch-blob/index.js",
+    "Require cycle: node_modules/react-native-fetch-blob/index.js -> node_modules/react-native-fetch-blob/polyfill/index.js -> node_modules/react-native-fetch-blob/polyfill/Fetch.js -> node_modules/react-native-fetch-blob/index.js",
+]);
+
 import App from "./App";
 import { name as appName } from "./app.json";
 
