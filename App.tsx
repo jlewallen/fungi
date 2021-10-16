4cb32df69d14a1d4604b7a98d380a0c2eb08854f
Jacob Lewallen <jlewallen@gmail.com>
2021-10-16 09:45:19 -0700

    style: Add .prettierrc

--- App.tsx
+++ App.tsx
@@ -8,246 +8,224 @@
  * @format
  */
 
-import React, {useState} from 'react';
-import {
-  SafeAreaView,
-  Switch,
-  Button,
-  StatusBar,
-  StyleSheet,
-  Text,
-  FlatList,
-  useColorScheme,
-  View,
-} from 'react-native';
-import {HStack, Checkbox, Center, NativeBaseProvider, Box} from 'native-base';
+import React, { useState } from "react";
+import { SafeAreaView, Switch, Button, StatusBar, StyleSheet, Text, FlatList, useColorScheme, View } from "react-native";
+import { HStack, Checkbox, Center, NativeBaseProvider, Box } from "native-base";
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
-import {Discovery} from './src/discovery';
-import moment from 'moment';
+import { Discovery } from "./src/discovery";
+import moment from "moment";
 
 const discovery = new Discovery();
 
 export class Station {
-  public readonly address: string;
+    public readonly address: string;
 
-  constructor(
-    public readonly id: string,
-    public readonly addresses: string[],
-    public readonly port: number,
-    public readonly zeroconf: Date | null,
-    public readonly udp: Date | null,
-    public readonly lost: Date | null,
-    public readonly queried: Date | null,
-    public readonly replied: Date | null,
-  ) {
-    this.address = this.addresses[0];
-  }
+    constructor(
+        public readonly id: string,
+        public readonly addresses: string[],
+        public readonly port: number,
+        public readonly zeroconf: Date | null,
+        public readonly udp: Date | null,
+        public readonly lost: Date | null,
+        public readonly queried: Date | null,
+        public readonly replied: Date | null
+    ) {
+        this.address = this.addresses[0];
+    }
 }
 
 const StationItem: React.FC<{
-  station: Station;
-}> = ({children, station}) => {
-  const isDarkMode = useColorScheme() === 'dark';
+    station: Station;
+}> = ({ children, station }) => {
+    const isDarkMode = useColorScheme() === "dark";
 
-  const primaryColor = station.lost ? 'coral' : 'darkseagreen';
+    const primaryColor = station.lost ? "coral" : "darkseagreen";
 
-  const renderAge = (value: Date | null): string => {
-    if (!value) {
-      return 'Never';
-    }
-    return moment().diff(value, 'seconds') + ' seconds ago';
-  };
+    const renderAge = (value: Date | null): string => {
+        if (!value) {
+            return "Never";
+        }
+        return moment().diff(value, "seconds") + " seconds ago";
+    };
 
-  const sections = [];
+    const sections = [];
 
-  const pushAgeSection = (label: string, value: Date | null) => {
-    if (!value) {
-      return;
+    const pushAgeSection = (label: string, value: Date | null) => {
+        if (!value) {
+            return;
+        }
+        sections.push(
+            <Text
+                key={sections.length}
+                style={[
+                    styles.stationSeen,
+                    {
+                        color: isDarkMode ? Colors.white : Colors.black,
+                    },
+                ]}
+            >
+                {label}: {renderAge(value)}
+            </Text>
+        );
+    };
+
+    sections.push(
+        <Text
+            key={sections.length}
+            style={[
+                styles.stationTitle,
+                {
+                    backgroundColor: primaryColor,
+                    color: isDarkMode ? Colors.white : Colors.black,
+                },
+            ]}
+        >
+            {station.address}
+        </Text>
+    );
+    if (station.lost) {
+        sections.push(
+            <Text
+                key={sections.length}
+                style={[
+                    styles.stationSeen,
+                    {
+                        color: isDarkMode ? Colors.white : Colors.black,
+                    },
+                ]}
+            >
+                LOST: {renderAge(station.lost)}
+            </Text>
+        );
     }
     sections.push(
-      <Text
-        key={sections.length}
-        style={[
-          styles.stationSeen,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {label}: {renderAge(value)}
-      </Text>,
+        <Text
+            key={sections.length}
+            style={[
+                styles.stationId,
+                {
+                    color: isDarkMode ? Colors.white : Colors.black,
+                },
+            ]}
+        >
+            {station.id}
+        </Text>
     );
-  };
 
-  sections.push(
-    <Text
-      key={sections.length}
-      style={[
-        styles.stationTitle,
-        {
-          backgroundColor: primaryColor,
-          color: isDarkMode ? Colors.white : Colors.black,
-        },
-      ]}>
-      {station.address}
-    </Text>,
-  );
-  if (station.lost) {
+    pushAgeSection("ZeroConf", station.zeroconf);
+    pushAgeSection("UDP", station.udp);
+    pushAgeSection("Queried", station.queried);
+    pushAgeSection("Reply", station.replied);
+
+    const buttonContainerStyle = {
+        marginTop: 10,
+    };
+
     sections.push(
-      <Text
-        key={sections.length}
-        style={[
-          styles.stationSeen,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        LOST: {renderAge(station.lost)}
-      </Text>,
+        <View style={buttonContainerStyle} key={sections.length}>
+            <Button title="Query" onPress={() => discovery.query(station.id)} />
+        </View>
     );
-  }
-  sections.push(
-    <Text
-      key={sections.length}
-      style={[
-        styles.stationId,
-        {
-          color: isDarkMode ? Colors.white : Colors.black,
-        },
-      ]}>
-      {station.id}
-    </Text>,
-  );
 
-  pushAgeSection('ZeroConf', station.zeroconf);
-  pushAgeSection('UDP', station.udp);
-  pushAgeSection('Queried', station.queried);
-  pushAgeSection('Reply', station.replied);
-
-  const buttonContainerStyle = {
-    marginTop: 10,
-  };
-
-  sections.push(
-    <View style={buttonContainerStyle} key={sections.length}>
-      <Button title="Query" onPress={() => discovery.query(station.id)} />
-    </View>,
-  );
-
-  return (
-    <View style={styles.stationContainer}>
-      {sections}
-      <Text
-        style={[
-          styles.stationDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+    return (
+        <View style={styles.stationContainer}>
+            {sections}
+            <Text
+                style={[
+                    styles.stationDescription,
+                    {
+                        color: isDarkMode ? Colors.light : Colors.dark,
+                    },
+                ]}
+            >
+                {children}
+            </Text>
+        </View>
+    );
 };
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+    const isDarkMode = useColorScheme() === "dark";
 
-  const containerStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-    flex: 1,
-  };
+    const containerStyle = {
+        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+        flex: 1,
+    };
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+    const backgroundStyle = {
+        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    };
 
-  const [isPassive, setIsPassive] = useState<boolean>(true);
-  const [stations, setStations] = useState<Station[]>([]);
+    const [isPassive, setIsPassive] = useState<boolean>(true);
+    const [stations, setStations] = useState<Station[]>([]);
 
-  const togglePassive = () => {
-    setIsPassive(!isPassive);
-  };
+    const togglePassive = () => {
+        setIsPassive(!isPassive);
+    };
 
-  discovery.start(async services => {
-    const stations = services.map(
-      s =>
-        new Station(
-          s.name,
-          s.addresses,
-          s.port,
-          s.zeroconf,
-          s.udp,
-          s.lost,
-          s.queried,
-          s.replied,
-        ),
+    discovery.start(async (services) => {
+        const stations = services.map((s) => new Station(s.name, s.addresses, s.port, s.zeroconf, s.udp, s.lost, s.queried, s.replied));
+        console.log("stations:", stations);
+        setStations(stations);
+        return Promise.resolve();
+    });
+
+    return (
+        <NativeBaseProvider>
+            <SafeAreaView style={containerStyle}>
+                <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
+                <HStack space={6} marginTop={5} marginLeft={5} marginRight={5} style={backgroundStyle}>
+                    <Switch
+                        accessibilityLabel="Passive networking mode."
+                        trackColor={{ false: "#767577", true: "#81b0ff" }}
+                        thumbColor={isPassive ? "#f5dd4b" : "#f4f3f4"}
+                        ios_backgroundColor="#3e3e3e"
+                        onValueChange={togglePassive}
+                        value={isPassive}
+                    />
+                    <Text style={{ fontWeight: "800" }}>Passive Networking</Text>
+                </HStack>
+                <FlatList
+                    style={backgroundStyle}
+                    data={stations}
+                    keyExtractor={(item) => item.id}
+                    renderItem={(row) => <StationItem station={row.item}></StationItem>}
+                />
+            </SafeAreaView>
+        </NativeBaseProvider>
     );
-    console.log('stations:', stations);
-    setStations(stations);
-    return Promise.resolve();
-  });
-
-  return (
-    <NativeBaseProvider>
-      <SafeAreaView style={containerStyle}>
-        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-        <HStack
-          space={6}
-          marginTop={5}
-          marginLeft={5}
-          marginRight={5}
-          style={backgroundStyle}>
-          <Switch
-            accessibilityLabel="Passive networking mode."
-            trackColor={{false: '#767577', true: '#81b0ff'}}
-            thumbColor={isPassive ? '#f5dd4b' : '#f4f3f4'}
-            ios_backgroundColor="#3e3e3e"
-            onValueChange={togglePassive}
-            value={isPassive}
-          />
-          <Text style={{fontWeight: '800'}}>Passive Networking</Text>
-        </HStack>
-        <FlatList
-          style={backgroundStyle}
-          data={stations}
-          keyExtractor={item => item.id}
-          renderItem={row => <StationItem station={row.item}></StationItem>}
-        />
-      </SafeAreaView>
-    </NativeBaseProvider>
-  );
 };
 
 const styles = StyleSheet.create({
-  stationContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  stationTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    padding: 5,
-  },
-  stationId: {
-    marginTop: 8,
-    fontSize: 14,
-    fontWeight: '200',
-  },
-  stationSeen: {
-    marginTop: 8,
-    fontSize: 14,
-    fontWeight: '400',
-  },
-  stationDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+    stationContainer: {
+        marginTop: 32,
+        paddingHorizontal: 24,
+    },
+    stationTitle: {
+        fontSize: 24,
+        fontWeight: "600",
+        padding: 5,
+    },
+    stationId: {
+        marginTop: 8,
+        fontSize: 14,
+        fontWeight: "200",
+    },
+    stationSeen: {
+        marginTop: 8,
+        fontSize: 14,
+        fontWeight: "400",
+    },
+    stationDescription: {
+        marginTop: 8,
+        fontSize: 18,
+        fontWeight: "400",
+    },
+    highlight: {
+        fontWeight: "700",
+    },
 });
 
 export default App;
--- src/discovery.ts
+++ src/discovery.ts
@@ -1,167 +1,160 @@
-import Zeroconf from 'react-native-zeroconf';
-import RNFetchBlob from 'react-native-fetch-blob';
+import Zeroconf from "react-native-zeroconf";
+import RNFetchBlob from "react-native-fetch-blob";
 
-import dgram from 'react-native-udp';
-import {Buffer} from 'buffer';
-import {fk_app as AppProto} from 'fk-app-protocol/fk-app';
+import dgram from "react-native-udp";
+import { Buffer } from "buffer";
+import { fk_app as AppProto } from "fk-app-protocol/fk-app";
 
 export class Registration {
-  constructor(
-    public readonly name: string,
-    public readonly addresses: string[],
-    public readonly port: number,
-    public zeroconf: Date | null = null,
-    public udp: Date | null = null,
-    public lost: Date | null = null,
-    public queried: Date | null = null,
-    public replied: Date | null = null,
-  ) {}
+    constructor(
+        public readonly name: string,
+        public readonly addresses: string[],
+        public readonly port: number,
+        public zeroconf: Date | null = null,
+        public udp: Date | null = null,
+        public lost: Date | null = null,
+        public queried: Date | null = null,
+        public replied: Date | null = null
+    ) {}
 }
 
 type CallbackFunc = (registrations: Registration[]) => Promise<void>;
 
 export class Discovery {
-  private readonly services: {[index: string]: Registration} = {};
-  private callback: CallbackFunc = async () => {};
-  private zeroconf: Zeroconf = null;
+    private readonly services: { [index: string]: Registration } = {};
+    private callback: CallbackFunc = async () => {};
+    private zeroconf: Zeroconf = null;
 
-  async start(callback: CallbackFunc): Promise<void> {
-    if (this.zeroconf) {
-      return;
-    }
-
-    this.callback = callback;
-    this.zeroconf = new Zeroconf();
-
-    this.zeroconf.on('start', () => console.log('Zeroconf scan has started.'));
-    this.zeroconf.on('stop', () => console.log('Zeroconf scan has stopped.'));
-    this.zeroconf.on('found', (found: unknown) =>
-      console.log('Zeroconf found.', found),
-    );
-    this.zeroconf.on('update', () => {
-      // console.log('Zeroconf update.');
-      this.refresh();
-    });
-    this.zeroconf.on('error', (error: string) =>
-      console.log('Zeroconf error.', error),
-    );
-
-    this.zeroconf.on(
-      'resolved',
-      (resolved: {name: string; addresses: string[]; port: number}) => {
-        console.log('Zeroconf found.', resolved);
-        this.onZeroConfFound(resolved.name, resolved.addresses, resolved.port);
-        this.refresh();
-      },
-    );
-
-    this.zeroconf.on('remove', (removed: string) => {
-      console.log('Zeroconf remove.', removed);
-      this.onServiceLost(removed);
-      this.refresh();
-    });
-
-    this.zeroconf.scan('fk', 'tcp', 'local.');
-
-    const socket = dgram.createSocket('udp4');
-    socket.bind(22143, function () {
-      console.log('udp: bound');
-      socket.addMembership('224.1.2.3');
-    });
-
-    socket.on('message', (buffer, rinfo) => {
-      const addresses = [rinfo.address];
-      console.log('udp: message', buffer, addresses);
-      const decoded = AppProto.UdpMessage.decodeDelimited(buffer);
-      const deviceId = Buffer.from(decoded.deviceId).toString('hex');
-      console.log('udp: decoded', decoded);
-      this.onUdpFound(deviceId, addresses, 80);
-    });
-
-    const periodicRefresh = () => {
-      setTimeout(() => {
-        this.refresh();
-        periodicRefresh();
-      }, 1000);
-    };
-
-    periodicRefresh();
-
-    return Promise.resolve();
-  }
-
-  refresh() {
-    this.callback(this.getServices());
-  }
-
-  onUdpFound(name: string, addresses: string[], port: number) {
-    console.log('onUdpFound', name, addresses, port);
-    if (!this.services[name]) {
-      this.services[name] = new Registration(name, addresses, port);
-    }
-    this.services[name].udp = new Date();
-    this.services[name].lost = null;
-  }
-
-  onZeroConfFound(name: string, addresses: string[], port: number) {
-    console.log('onZeroConfFound', name, addresses, port);
-    if (!this.services[name]) {
-      this.services[name] = new Registration(name, addresses, port);
-    }
-    this.services[name].zeroconf = new Date();
-    this.services[name].lost = null;
-  }
-
-  onServiceLost(name: string) {
-    if (this.services[name]) {
-      console.log('onServiceLost', name);
-      this.services[name].lost = new Date();
-    } else {
-      console.log('onServiceLost (MYSTERY)', name);
-    }
-  }
-
-  getServices(): Registration[] {
-    return Object.values(this.services);
-  }
-
-  async query(name: string): Promise<void> {
-    const service = this.services[name];
-    if (!service) {
-      console.log('querying (MYSTERY)', name);
-      return Promise.resolve();
-    }
-
-    console.log('query-service', service);
-
-    const url = `http://${service.addresses[0]}:${service.port}/fk/v1`;
-
-    console.log('query-url', url);
-
-    service.queried = new Date();
-
-    this.refresh();
-
-    await RNFetchBlob.fetch('GET', url)
-      .then((res: ResponseType) => {
-        service.replied = new Date();
-        console.log('response', res.info());
-        const status = res.info().status;
-        if (status == 200) {
-          const base64Str = res.base64();
-          const buffer = Buffer.from(base64Str, 'base64');
-          const decoded = AppProto.HttpReply.decodeDelimited(buffer);
-          console.log('query-decoded', decoded);
+    async start(callback: CallbackFunc): Promise<void> {
+        if (this.zeroconf) {
+            return;
         }
-      })
-      .catch((errorMessage: string, statusCode: number) => {
-        console.log(`query-error-`, statusCode, errorMessage);
-      });
 
-    this.refresh();
+        this.callback = callback;
+        this.zeroconf = new Zeroconf();
 
-    return Promise.resolve();
-  }
+        this.zeroconf.on("start", () => console.log("Zeroconf scan has started."));
+        this.zeroconf.on("stop", () => console.log("Zeroconf scan has stopped."));
+        this.zeroconf.on("found", (found: unknown) => console.log("Zeroconf found.", found));
+        this.zeroconf.on("update", () => {
+            // console.log('Zeroconf update.');
+            this.refresh();
+        });
+        this.zeroconf.on("error", (error: string) => console.log("Zeroconf error.", error));
+
+        this.zeroconf.on("resolved", (resolved: { name: string; addresses: string[]; port: number }) => {
+            console.log("Zeroconf found.", resolved);
+            this.onZeroConfFound(resolved.name, resolved.addresses, resolved.port);
+            this.refresh();
+        });
+
+        this.zeroconf.on("remove", (removed: string) => {
+            console.log("Zeroconf remove.", removed);
+            this.onServiceLost(removed);
+            this.refresh();
+        });
+
+        this.zeroconf.scan("fk", "tcp", "local.");
+
+        const socket = dgram.createSocket("udp4");
+        socket.bind(22143, function () {
+            console.log("udp: bound");
+            socket.addMembership("224.1.2.3");
+        });
+
+        socket.on("message", (buffer, rinfo) => {
+            const addresses = [rinfo.address];
+            console.log("udp: message", buffer, addresses);
+            const decoded = AppProto.UdpMessage.decodeDelimited(buffer);
+            const deviceId = Buffer.from(decoded.deviceId).toString("hex");
+            console.log("udp: decoded", decoded);
+            this.onUdpFound(deviceId, addresses, 80);
+        });
+
+        const periodicRefresh = () => {
+            setTimeout(() => {
+                this.refresh();
+                periodicRefresh();
+            }, 1000);
+        };
+
+        periodicRefresh();
+
+        return Promise.resolve();
+    }
+
+    refresh() {
+        this.callback(this.getServices());
+    }
+
+    onUdpFound(name: string, addresses: string[], port: number) {
+        console.log("onUdpFound", name, addresses, port);
+        if (!this.services[name]) {
+            this.services[name] = new Registration(name, addresses, port);
+        }
+        this.services[name].udp = new Date();
+        this.services[name].lost = null;
+    }
+
+    onZeroConfFound(name: string, addresses: string[], port: number) {
+        console.log("onZeroConfFound", name, addresses, port);
+        if (!this.services[name]) {
+            this.services[name] = new Registration(name, addresses, port);
+        }
+        this.services[name].zeroconf = new Date();
+        this.services[name].lost = null;
+    }
+
+    onServiceLost(name: string) {
+        if (this.services[name]) {
+            console.log("onServiceLost", name);
+            this.services[name].lost = new Date();
+        } else {
+            console.log("onServiceLost (MYSTERY)", name);
+        }
+    }
+
+    getServices(): Registration[] {
+        return Object.values(this.services);
+    }
+
+    async query(name: string): Promise<void> {
+        const service = this.services[name];
+        if (!service) {
+            console.log("querying (MYSTERY)", name);
+            return Promise.resolve();
+        }
+
+        console.log("query-service", service);
+
+        const url = `http://${service.addresses[0]}:${service.port}/fk/v1`;
+
+        console.log("query-url", url);
+
+        service.queried = new Date();
+
+        this.refresh();
+
+        await RNFetchBlob.fetch("GET", url)
+            .then((res: ResponseType) => {
+                service.replied = new Date();
+                console.log("response", res.info());
+                const status = res.info().status;
+                if (status == 200) {
+                    const base64Str = res.base64();
+                    const buffer = Buffer.from(base64Str, "base64");
+                    const decoded = AppProto.HttpReply.decodeDelimited(buffer);
+                    console.log("query-decoded", decoded);
+                }
+            })
+            .catch((errorMessage: string, statusCode: number) => {
+                console.log(`query-error-`, statusCode, errorMessage);
+            });
+
+        this.refresh();
+
+        return Promise.resolve();
+    }
 }
 
 type ResponseType = {};
